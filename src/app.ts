9f283ab272fbc9b0a3672a26c4cc2a2f83fe91f1
HectorOrlando <14orgui@gmail.com>
2023-11-15 18:25:13 +0100

    añadir directorios principales

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+// import { Server } from "./infrastructure/servers/Server";
+
+// Crea una instancia del servidor.
+// const server = new Server();
+// Inicia el servidor y lo pone en escucha de solicitudes.
+// server.listen();
+
+/*
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://user_node_cafe:3VRWcT31d018aPAk@miclustercafe.0teuk4k.mongodb.net/productsDB', {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+*/
