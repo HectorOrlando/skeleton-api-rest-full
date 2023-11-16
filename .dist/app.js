73e4b4a277db2a9ec873e9a88b0aa18446b70aeb
HectorOrlando <14orgui@gmail.com>
2023-11-16 14:04:59 +0100

    modificar gitignore y tsconfig.json

--- .dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// app.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./infrastructure/servers/Server");
-const server = new Server_1.Server(); // Crea una instancia del servidor.
-server.listen(); // Inicia el servidor y lo pone en escucha de solicitudes.
